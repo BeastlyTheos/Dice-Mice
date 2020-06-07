f8da45a13f361b019d7c284ab496b9110648c488
Ted <TheodoreCooke@trentu.ca>
2020-06-07 15:10:12 -0400

    bot echos messages received

--- bot.py
+++ bot.py
@@ -5,11 +5,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-	print(f"{client.user} has connected to discord!")
+	print("connected.")
+
+
+@client.event
+async def on_message(msg):
+	if msg.author == client.user:
+		print(f"client just said {msg.content}")
+		return
+
+	await msg.channel.send(f"You just said {msg.content}")
 
 client.run(TOKEN)
